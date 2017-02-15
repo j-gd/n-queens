e9efbe943501f755c478950e0b9c01328037c7ad
Jonathan Chou <jonchou@ucdavis.edu>
2017-02-15 15:41:36 -0800

    Added minorDiagonalConflict check

--- src/Board.js
+++ src/Board.js
@@ -80,10 +80,8 @@
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
       var row = this.get(rowIndex);
-      // console.log('row is: ' + row);
       var count = 0;
       for (var i = 0; i < row.length; i++) {
-        // console.log('row i: ' + row[i]);
         if (row[i] === 1) {
           count++;
           if (count > 1) {
@@ -91,21 +89,18 @@
           }
         }
       }
-      return false; // fixme
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
       var n = this.get('n');
-      var hasConflict = false;
       for (var i = 0; i < n; i++) {
-        hasConflict = this.hasRowConflictAt(i);
-        if (hasConflict) {
-          return hasConflict;
+        if (this.hasRowConflictAt(i)) {
+          return true;
         }
       }
-      // return true;
-      return hasConflict; // false
+      return false;
     },
 
 
@@ -128,21 +123,19 @@
         }
       }
 
-      return false; // fixme
+      return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
       var n = this.get('n');
-      var hasConflict = false;
+
       for (var i = 0; i < n; i++) {
-        hasConflict = this.hasColConflictAt(i);
-        if (hasConflict) {
-          return hasConflict;
+        if (this.hasColConflictAt(i)) {
+          return true;
         }
       }
-
-      return hasConflict; // fixme
+      return false;
     },
 
 
@@ -152,12 +145,44 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var n = this.get('n');
+      // for each index index -1, +1 in the next row chek if it's a 1
+      var count = 0;  
+
+      // check diagonal from majordiagonalindex
+      var columnIndex = 0;
+
+      for (var i = majorDiagonalColumnIndexAtFirstRow; i < majorDiagonalColumnIndexAtFirstRow + n; i++) {
+        if (i < 0) {
+          columnIndex++;
+          continue;
+        } else if (i >= n) {
+          break;
+        }
+
+        var row = this.get(i);
+
+        if (row[columnIndex] === 1) {
+          count++;
+          if (count > 1) {
+            return true;
+          }
+        }
+
+        columnIndex++;
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = -n + 2; i < n-1; i++) {
+        if(this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -167,12 +192,44 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var n = this.get('n');
+      // for each index index -1, +1 in the next row chek if it's a 1
+      var count = 0;  
+      var rowIndex = 0;
+      for (var i = minorDiagonalColumnIndexAtFirstRow; i >= 0; i--) {
+        if (i >= n) {
+          rowIndex++;
+          continue;
+        } else if (i < 0) {
+          break;
+        }
+        // 0 0 0 0
+        // 0 0 0 0 
+        // 0 0 1 0
+        // 0 1 0 0
+        var row = this.get(rowIndex);
+
+        if (row[i] === 1) {
+          count++;
+          if (count > 1) {
+            return true;
+          }
+        }
+
+        rowIndex++;
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = 1; i < n+n-1; i++) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
